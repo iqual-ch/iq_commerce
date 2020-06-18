9fe50f5411dc7c1ee641e37806a683a7ae8be9a2
danielwirz <d.wirz@iqual.ch>
2020-06-18 15:02:29 +0200

    added ICartBlock as extension to CartBlock

---
+++ src/Plugin/Block/IqCartBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\iq_commerce\Plugin\Block;
+
+use Drupal\commerce_cart\Plugin\Block\CartBlock;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Extendeds CartBlock with a configurable title.
+ *
+ * @Block(
+ *   id = "iq_commerce_cart",
+ *   admin_label = @Translation("Dropdown Cart"),
+ *   category = @Translation("Commerce")
+ * )
+ */
+class IqCartBlock extends CartBlock {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $form['cart_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#default_value' => isset($this->configuration['cart_title']) ? $this->configuration['cart_title'] : 'Cart (%count)',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $render = parent::build();
+    $render['#count_text'] = $this->t($this->configuration['cart_title'], [
+      '%count' => $render['#count'],
+    ]);
+    return $render;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $this->configuration['cart_title'] = $form_state->getValue('cart_title');
+  }
+
+
+}
---
+++ src/Plugin/views/field/AlterCartQuantityField.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\iq_commerce\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to flag the node type.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("alter_cart_quantity_field")
+ */
+class AlterCartQuantityField extends FieldPluginBase {
+
+  /**
+   * @{inheritdoc}
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * Define the available options.
+   *
+   * @return array
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    $options['btn_delete_label'] = ['default' => $this->t('Remove')];
+
+    return $options;
+  }
+
+  /**
+   * Provide the options form.
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    $form['btn_delete_label'] = [
+      '#title' => $this->t('Tooltip remove button'),
+      '#type' => 'textfield',
+      '#default_value' => $this->options['btn_delete_label'],
+
+    ];
+
+    parent::buildOptionsForm($form, $form_state);
+  }
+
+  /**
+   * @{inheritdoc}
+   */
+  public function render(ResultRow $values) {
+
+    return [
+      '#type' => 'inline_template',
+      '#template' => '<button data-drupal-selector="edit-remove-button-{{ id }}" type="submit" id="edit-remove-button-{{ id }}" name="delete-order-item-{{ id }}" title="{{name}}"><i class="far fa-trash-alt"></i></button>',
+      '#remove_order_item' => TRUE,
+      '#row_index' => $values->index,
+      '#context' => [
+        'id'  => $values->index,
+        'name' => $this->options['btn_delete_label'],
+      ],
+    ];
+
+  }
+
+}
