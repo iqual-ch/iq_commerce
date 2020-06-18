d3a082e79214a32222ef96bd79eefa166f85a62e
Ginovski <i.ginovski@iqual.ch>
2020-06-18 16:10:19 +0200

    Fixes in the tax rate

--- src/Resolver/ProductTaxResolver.php
+++ src/Resolver/ProductTaxResolver.php
@@ -3,6 +3,7 @@
 namespace Drupal\iq_commerce\Resolver;
 
 use Drupal\commerce_product\Entity\ProductInterface;
+use Drupal\commerce_product\Entity\ProductType;
 use Drupal\commerce_product\Entity\ProductVariationInterface;
 use Drupal\commerce_order\Entity\OrderItemInterface;
 use Drupal\commerce_tax\Resolver\TaxRateResolverInterface;
@@ -24,9 +25,18 @@ class ProductTaxResolver implements TaxRateResolverInterface {
     $item = $order_item->getPurchasedEntity();
 
     // Get the corresponding product.
+    /** @var \Drupal\commerce_product\Entity\Product $product */
     $product = $item->getProduct();
+    $product_type = ProductType::load($product->bundle());
+    $rate_id = $product_type->getThirdPartySetting('iq_commerce', 'tax_rate');
 
-    $product_type = $product->bundle();
+    // Check for a tax rate field in the product type.
+    foreach ($rates as $rate) {
+      if ($rate->getId() == $rate_id) {
+        return $rate;
+      }
+    }
+    /*$product_type = $product->bundle();
     $config = \Drupal::config('iq_commerce.settings');
     $savedTaxRates = $config->get('tax_rate_per_product');
 
@@ -42,14 +52,10 @@ class ProductTaxResolver implements TaxRateResolverInterface {
           // rate resolver.
           return NULL;
       }
-    }
-    foreach ($rates as $rate) {
-      if ($rate->getId() == $rate_id) {
-        return $rate;
-      }
-    }
+    }*/
 
-    // If no rate has been found, let's others resolvers try to get it.
+
+    // If no rate has been found, let the other resolvers try to get it.
     return NULL;
   }
 
