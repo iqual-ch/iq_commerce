1bdc9cb774f411285a7dd315b0b35cf32781f8a6
Ginovski <i.ginovski@iqual.ch>
2020-09-20 15:34:18 +0200

    Added AgeConfirmationPane in the checkout process

---
+++ src/Plugin/Commerce/CheckoutPane/AgeConfirmationPane.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\iq_commerce\Plugin\Commerce\CheckoutPane;
+
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a checkout message pane.
+ *
+ * @CommerceCheckoutPane(
+ *   id = "iq_commerce_pane_age_confirmation",
+ *   label = @Translation("Age confirmation"),
+ * )
+ */
+class AgeConfirmationPane extends CheckoutPaneBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
+    $pane_form['age_confirmation'] = [
+      '#type' => 'checkbox',
+      '#required' => true,
+      '#title' => 'I confirm that I am over 18 years old.'
+    ];
+    return $pane_form;
+  }
+
+
+}
