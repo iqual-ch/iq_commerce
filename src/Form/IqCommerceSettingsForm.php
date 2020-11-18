682734eb2c481e3603cc0dad3b12b0b73d5a674a
Ivan Ginovski <i.ginovski@iqual.ch>
2020-11-18 15:55:23 +0100

    Added swiftmailer, config for footer and header in emails, fixes in config, translations and checkout panes

---
+++ src/Form/IqCommerceSettingsForm.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\iq_commerce\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\iq_commerce\Controller\UserController;
+use Drupal\user\Entity\Role;
+use Drupal\user\Entity\User;
+use Drupal\user\RoleInterface;
+
+/**
+ * Class IqCommerceSettingsForm.
+ *
+ * @package Drupal\iq_commerce\Form
+ */
+class IqCommerceSettingsForm extends ConfigFormBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    return 'iq_commerce_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $iqCommerceSettings = $this->getIqCommerceSettings();
+    $form['header'] = [
+      '#type'       => 'text_format',
+      '#format' => $iqCommerceSettings['header']['format'],
+      '#title' => $this->t('Header'),
+      '#description' => $this->t('Add a header message for the receipt.'),
+      '#default_value' => $iqCommerceSettings['header']['value'],
+    ];
+
+    $form['footer'] = [
+      '#type'       => 'text_format',
+      '#format' => $iqCommerceSettings['footer']['format'],
+      '#title' => $this->t('Footer'),
+      '#description' => $this->t('Add a footer message for the receipt.'),
+      '#default_value' => $iqCommerceSettings['footer']['value'],
+    ];
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $this->config('iq_commerce.settings')
+      ->set('header', $form_state->getValue('header'))
+      ->set('footer', $form_state->getValue('footer'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Get Editable config names.
+   *
+   * @inheritDoc
+   */
+  protected function getEditableConfigNames()
+  {
+    return ['iq_commerce.settings'];
+  }
+
+  /**
+   * Helper function to get the iq_commerce settings.
+   */
+  public static function getIqCommerceSettings() {
+    $iqCommerceSettingsConfig = \Drupal::config('iq_commerce.settings');
+    return [
+      'header' => $iqCommerceSettingsConfig->get('header') != NULL ? $iqCommerceSettingsConfig->get('header') : ['value' => '<b>' . t("Commerce Store.") . '</b>', 'format' => 'pagedesigner'],
+      'footer' => $iqCommerceSettingsConfig->get('footer') != NULL ? $iqCommerceSettingsConfig->get('footer') : ['value' => t("Thank you for your order. You will receive the item(s) in 3 - 7 days."), 'format' => 'pagedesigner'],
+    ];
+  }
+
+
+}
--- src/Plugin/Commerce/CheckoutPane/CheckoutMessagePane.php
+++ src/Plugin/Commerce/CheckoutPane/CheckoutMessagePane.php
@@ -19,19 +19,17 @@ class CheckoutMessagePane extends CheckoutPaneBase {
    * {@inheritdoc}
    */
   public function buildPaneForm(array $pane_form, FormStateInterface $form_state, array &$complete_form) {
+    $message = $this->t("<h1>Thank you for your order.</h1>The order number is : @order_number<br/>",['@order_number' => $this->order->getOrderNumber()]);
+    if (\Drupal::currentUser()->isAuthenticated()) {
+      $message .= $this->t("You can view your orders <a href='/user/orders'>here</a><br/>");
+    }
+    else {
+      $message .= $this->t("You can see your order when you are logged in.<br/>Continue to <a href='/user/login'>login</a><br/>");
+    }
+    $message .= $this->t("Continue to <a href='/de'>homepage</a><br/>If you have any questions, you can reach us at <a href='/contact'>Contact</a>");
+
     $pane_form['message'] = [
-      '#markup' => $this->t("
-      <H1>Thank you for your order.</H1>
-
-The order number is : @order_number<br/> 
-
-You can see your order when you are logged in.<br/>
-
-Continue to <a href='/user/login'>login</a><br/>
-
-Continue to <a href='/de'>homepage</a><br/>
-
-If you have any questions, you can reach us at <a href='/contact'>Contact</a>", ['@order_number' => $this->order->getOrderNumber()]),
+      '#markup' => $this->t($message)
     ];
     return $pane_form;
   }
