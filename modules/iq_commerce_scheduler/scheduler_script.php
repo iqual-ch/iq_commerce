94839ae1860cd000b4d2bda468742a4516a6bcbf
ignovski <i.ginovski@iqual.ch>
2021-03-16 15:46:20 +0100

    Switched scheduler from product to product_variation level

--- modules/iq_commerce_scheduler/scheduler_script.php
+++ modules/iq_commerce_scheduler/scheduler_script.php
@@ -1,10 +1,12 @@
 <?php
 
+use Drupal\commerce_product\Entity\ProductVariation;
+
 die();
-$products_to_be_published = \Drupal::entityQuery('commerce_product')->condition('field_iq_commerce_publish_on', NULL, 'IS NOT NULL')
+$products_to_be_published = \Drupal::entityQuery('commerce_product_variation')->condition('field_iq_commerce_publish_on', NULL, 'IS NOT NULL')
   ->condition('status', 0)->execute();
 foreach ($products_to_be_published as $key => $value) {
-  $product = Product::load($value);
+  $product = ProductVariation::load($value);
   $publish_date = $product->field_iq_commerce_publish_on->value;
 
   if ($publish_date < time()) {
@@ -13,8 +15,8 @@ foreach ($products_to_be_published as $key => $value) {
   }
 }
 die();
-/** @var \Drupal\commerce_product\Entity\Product $product */
-$product = \Drupal\commerce_product\Entity\Product::load(852);
+/** @var \Drupal\commerce_product\Entity\ProductVariation $product */
+$product = ProductVariation::load(852);
 $cart_expiration = $product->getThirdPartySetting('iq_commerce_scheduler', 'publish_on');
 var_dump($cart_expiration);
 $timestamp = time();
