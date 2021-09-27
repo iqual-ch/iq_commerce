1810824ed1da6073c5fa867055e6b7d91173883a
GinovskiIvan <ginovski.ivan@gmail.co>
2021-09-27 10:01:48 +0200

    Initial version with dependency products cart synchronization

--- modules/iq_commerce_required_product/src/EventSubscriber/RequiredProductEventSubscriber.php
+++ modules/iq_commerce_required_product/src/EventSubscriber/RequiredProductEventSubscriber.php
@@ -3,14 +3,17 @@
 namespace Drupal\iq_commerce_required_product\EventSubscriber;
 
 use Drupal\commerce\PurchasableEntityInterface;
+use Drupal\commerce_cart\CartManagerInterface;
 use Drupal\commerce_cart\Event\CartEvents;
 use Drupal\commerce_cart\Event\CartOrderItemRemoveEvent;
 use Drupal\commerce_cart\Event\CartOrderItemUpdateEvent;
+use Drupal\commerce_order\Entity\OrderItem;
 use Drupal\commerce_product\Entity\Product;
 use Drupal\commerce_product\Entity\ProductVariation;
 use Drupal\Component\Serialization\Yaml;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\iq_commerce\Event\IqCommerceAfterCartAddEvent;
 use Drupal\iq_commerce\Event\IqCommerceBeforeCartAddEvent;
 use Drupal\iq_commerce\Event\IqCommerceCartEvents;
 use Drupal\iq_commerce\Form\IqCommerceProductSettingsForm;
@@ -34,17 +37,27 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
   protected $entityRepository;
 
   /**
+   * The cart manager.
+   *
+   * @var \Drupal\commerce_cart\CartManagerInterface
+   */
+  protected $cartManager;
+
+  /**
    * Constructs a new RequiredProductEventSubscriber object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
    *   The entity repository.
+   * @param \Drupal\commerce_cart\CartManagerInterface $cart_manager
+   *   The cart manager.
    *
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager,EntityRepositoryInterface $entity_repository) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager,EntityRepositoryInterface $entity_repository, CartManagerInterface $cart_manager) {
     $this->entityTypeManager = $entity_type_manager;
     $this->entityRepository = $entity_repository;
+    $this->cartManager = $cart_manager;
   }
 
   /**
@@ -53,6 +66,7 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     $events = [
       IqCommerceCartEvents::BEFORE_CART_ENTITY_ADD => 'addRequiredProducts',
+      IqCommerceCartEvents::AFTER_CART_ENTITY_ADD => 'lockRequiredProducts',
       CartEvents::CART_ORDER_ITEM_UPDATE => 'updateRequiredProducts',
       CartEvents::CART_ORDER_ITEM_REMOVE => 'removeRequiredProducts'
     ];
@@ -137,6 +151,24 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
     }
   }
 
+  public function lockRequiredProducts(IqCommerceAfterCartAddEvent $event) {
+    \Drupal::logger('iq_commerce_required_product')->notice('entered the after acard event');
+    $response_data = $event->getResponseWithAdditionalData();
+    \Drupal::logger('iq_commerce_required_product')->notice(json_encode(array_keys($response_data->getResponseData())));
+    $dependency_items = [];
+    foreach ($response_data->getResponseData()['order_items'] as $required_item) {
+      $dependency_items = array_merge($this->getRequiredVariations($required_item), $dependency_items);
+    }
+
+    foreach ($response_data->getResponseData()['order_items'] as $required_item) {
+      $dependency_items = $this->getRequiredVariations($required_item);
+      \Drupal::logger('iq_commerce_required_product')->notice(json_encode($required_item->getPurchasedEntity()->label()));
+      if ($required_item->getPurchasedEntity()->getSku()) {
+        $required_item->setData('lock', TRUE)->save();
+      }
+    }
+  }
+
   /**
    *
    */
@@ -144,6 +176,7 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
     $order_item = $event->getOrderItem();
     /** @var ProductVariation $variation */
     $variation = $order_item->getPurchasedEntity();
+    $variation->getSku()
 
     $requiredItems = $this->getRequiredVariations($variation, $order_item->getQuantity());
     $cart = $order_item->getOrder();
@@ -153,9 +186,11 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
        * @var ProductVariation $requiredItem
        */
       foreach ($requiredItems as $key => $requiredItem) {
-        if ($requiredItem['required_product']->getSku() == $item->getPurchasedEntity()->getSku()) {
+        if (!empty($requiredItem['required_product']) && $requiredItem['required_product']->getSku() == $item->getPurchasedEntity()->getSku() && $item->getQuantity() != $requiredItem['quantity']) {
           $item->setQuantity($requiredItem['quantity']);
-          $item->save();
+          $this->cartManager->updateOrderItem($cart, $item, TRUE);
+          var_dump($item->getData('lock'));
+          die();
         }
       }
     }
@@ -195,6 +230,7 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
     $iqCommerceProductSettingsConfig = RequiredProductSettingsForm::getSettings();
     $required_field_names = $iqCommerceProductSettingsConfig['required_product_fields'];
     $added_products = [];
+    $i = 0;
 
     /** If the required product is not in the cart, add it to the body. */
     foreach ($required_field_names as $required_field_name => $field_settings) {
@@ -217,22 +253,20 @@ class RequiredProductEventSubscriber implements EventSubscriberInterface {
               $required_product = ProductVariation::load($required_product['target_id']);
             }
             if (!empty($required_product)) {
-              $added_products[]['required_product'] = $required_product;
-              if (!empty($required_product)) {
-                $added_products[] = $required_product;
-                if ($field_settings['sync_quantities']) {
-                  $quantity =  (!empty($purchased_entity_quantity)) ? $purchased_entity_quantity : 1;
-                }
-                else {
-                  /*
-                   * @TODO Get the cart and iterate through the items to check
-                   * whether the required product is already there and skip to
-                   * next item if it is.
-                  */
-                  $quantity = 1;
-                }
-                $added_products[]['quantity'] = $quantity;
+              $added_products[$i]['required_product'] = $required_product;
+              if ($field_settings['sync_quantities']) {
+                $quantity =  (!empty($purchased_entity_quantity)) ? $purchased_entity_quantity : 1;
               }
+              else {
+                /*
+                 * @TODO Get the cart and iterate through the items to check
+                 * whether the required product is already there and skip to
+                 * next item if it is.
+                */
+                $quantity = 1;
+              }
+              $added_products[$i]['quantity'] = $quantity;
+              $i++;
             }
           }
         }
