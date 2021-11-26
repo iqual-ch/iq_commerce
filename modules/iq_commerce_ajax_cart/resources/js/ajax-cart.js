b1d249cef5f4f33aa49aa49df25747305b55237d
Daniel Wirz <d.wirz@iqual.ch>
2021-11-26 20:17:18 +0100

    fix: change deprecated crsfToken route

--- modules/iq_commerce_ajax_cart/resources/js/ajax-cart.js
+++ modules/iq_commerce_ajax_cart/resources/js/ajax-cart.js
@@ -7,7 +7,7 @@
    */
   Drupal.behaviors.iq_commerce_ajax_cart = {
     getCsrfToken: function (callback) {
-      $.get(Drupal.url('rest/session/token'))
+      $.get(Drupal.url('/session/token'))
         .done(function (data) {
           callback(data);
         });
