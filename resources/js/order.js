cc9ee9a3c67dc4ead3b8e97dbb4d579b121f1246
Erik Kuznik <erik.kuznik@gmail.com>
2022-12-01 20:51:25 +0000

    DCFI-199: User-related code moved to iq_user_base

--- resources/js/order.js
+++ resources/js/order.js
@@ -1,11 +1,4 @@
 (function ($, Drupal) {
-/* User login menu dropdown (MyAccount) */
-  $('.nav-holder').hover(function () {
-    const dropdownMenu = $(this).children('.nav-holder > .nav');
-    if (dropdownMenu.is(':visible')) {
-      dropdownMenu.toggleClass('expanded');
-    }
-  });
 
   /**
    * Back button on order detail pages with fallback
