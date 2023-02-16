2a0ef1cb91ea20c7ad45c7f5ad94c3e3bd26f3be
Erik Kuznik <erik.kuznik@gmail.com>
2023-02-16 09:28:20 +0000

    Remove comments and change class notation to BEM

--- resources/js/variation_add_to_cart_form.js
+++ resources/js/variation_add_to_cart_form.js
@@ -2,13 +2,13 @@
   Drupal.behaviors.iq_commerce_variation_add_to_cart_form = {
     attach: function (context, settings) {
 
-      $(context).find('button.plus').on('click', function () {
-        const $input = $(this).siblings('input[type="number"].quantity');
+      $(context).find('commerce-variation-add-to-cart__quantity-button--plus').on('click', function () {
+        const $input = $(this).siblings('input[type="number"].commerce-variation-add-to-cart__quantity-input');
         $input.val(Math.max(0, parseInt($input.val()) + 1));
       })
 
-      $(context).find('button.minus').on('click', function () {
-        const $input = $(this).siblings('input[type="number"].quantity');
+      $(context).find('commerce-variation-add-to-cart__quantity-button--minus').on('click', function () {
+        const $input = $(this).siblings('input[type="number"].commerce-variation-add-to-cart__quantity-input');
         $input.val(Math.max(0, parseInt($input.val()) - 1));
       });
 
