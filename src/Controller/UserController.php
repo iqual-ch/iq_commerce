c3dd54add0e8547941f6cf478780a7be404cd291
Guillaume Pacilly <guillaume.pacilly@gmail.com>
2026-01-12 13:21:46 +0000

    DUP-172: Drupal 11 Upgrade - proper redirect for anonymous users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,24 +7,53 @@ use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
- * IQ Commerce User account setting controller.
+ * IQ Commerce User controller.
+ *
+ * Redirect anonymous users to login page
+ * before accessing user-specific pages.
  */
 class UserController extends ControllerBase {
+
+  /**
+   * Redirect to current user's edit page.
+   */
+  public function userEditRedirect() {
+    // If user is not logged in, redirect to login page.
+    if ($this->currentUser()->isAnonymous()) {
+      $login_url = Url::fromRoute('user.login', [], [
+        'query' => [
+          'destination' => '/user/edit',
+        ],
+      ]);
+      $response = new RedirectResponse($login_url->toString(), 302);
+      return $response;
+    }
+
+    // Redirect to the user's edit page.
+    $user_id = $this->currentUser()->id();
+    $response = new RedirectResponse(Url::fromRoute('entity.user.edit_form', ['user' => $user_id])->toString(), 302);
+    return $response;
+  }
 
   /**
    * Redirect to current user's orders page.
    */
   public function userOrdersRedirect() {
-    $user_id = $this->currentUser()->id();
 
     // If user is not logged in, redirect to login page.
-    if ($user_id == 0) {
-      $response = new RedirectResponse(Url::fromRoute('user.login')->toString(), 302);
+    if ($this->currentUser()->isAnonymous()) {
+      $login_url = Url::fromRoute('user.login', [], [
+        'query' => [
+          'destination' => '/user/orders',
+        ],
+      ]);
+      $response = new RedirectResponse($login_url->toString(), 302);
       return $response;
     }
 
     // Redirect to the user's orders page.
-    $response = new RedirectResponse(Url::fromUserInput('/user/' . $user_id . '/orders')->toString(), 302);
+    $user_id = $this->currentUser()->id();
+    $response = new RedirectResponse(Url::fromRoute('user.orders', ['user' => $user_id])->toString(), 302);
     return $response;
   }
 
